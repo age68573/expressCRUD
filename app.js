3a1bad0a77c1c0de090393aa3baa9f676b52fb2b
age68573 <s1410523045@gms.nutc.edu.tw>
2021-06-03 14:04:11 +0800

    install bootstrap & adjust index css

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express()
 
+app.use('/node_modules/', express.static('./node_modules/'))
+app.use('/public/', express.static('./public/'))
+
 app.engine('html', require('express-art-template'))
 
 app.get('/', (req, res) => {
