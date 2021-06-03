2debcc378547ac24dafea72ac5b9359af1ea8bf4
age68573 <s1410523045@gms.nutc.edu.tw>
2021-06-03 15:58:42 +0800

    使用express Router API 封裝 router

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*
+  創建服務
+  服務相關配置
+    模板引擎
+    body-parser 解析 POST 請求
+    提供靜態服務資源
+  掛載路由
+  監聽端口
+*/
+
 const express = require('express')
 const router = require('./router')
 
@@ -8,7 +18,8 @@ app.use('/public/', express.static('./public/'))
 
 app.engine('html', require('express-art-template'))
 
-router(app)
+// 把路由容器掛載到 app 服務中
+app.use(router)
 
 app.listen(3000, () => {
   console.log('express app running...');
--- router.js
+++ router.js
@@ -1,25 +1,66 @@
-module.exports = function (app) {
-  const fs = require('fs')
-  app.get('/students', (req, res) => {
-    // 讀取 JSON 文件 (第二個參數可以轉成 UTF-8 編碼 獲通過 .toString())
-    fs.readFile('./db.json', 'utf-8', (err, data) => {
-      if (err) {
-        return res.status(500).send('Server Error.')
-      }
-      // console.log(JSON.parse(data));
-      res.render('index.html', {
-        colors: [
-          'red',
-          'yellow',
-          'blue',
-          'green'
-        ],
-        students: JSON.parse(data).students
-      })
+/*
+ * 處理路由
+ * 根據不同的方法請求設置不同的處理函數
+*/
+const fs = require('fs')
+
+// express 提供了封裝路由的方式
+const express = require('express')
+
+// 1. 創建路由容器
+const router = express.Router()
+
+// 2.把路由都掛載到 router 路由容器中
+router.get('/students', (req, res) => {
+  // 讀取 JSON 文件 (第二個參數可以轉成 UTF-8 編碼 獲通過 .toString())
+  fs.readFile('./db.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Server Error.')
+    }
+    // console.log(JSON.parse(data));
+    res.render('index.html', {
+      colors: [
+        'red',
+        'yellow',
+        'blue',
+        'green'
+      ],
+      students: JSON.parse(data).students
     })
   })
-  app.get('/students/new', (req, res) => {
+})
+router.get('/students/new', (req, res) => {
 
-  })
-}
+})
+
+// 3. 將 router 導出
+module.exports = router
+
+
+
+
+
+// module.exports = function (app) {
+//   app.get('/students', (req, res) => {
+//     // 讀取 JSON 文件 (第二個參數可以轉成 UTF-8 編碼 獲通過 .toString())
+//     fs.readFile('./db.json', 'utf-8', (err, data) => {
+//       if (err) {
+//         return res.status(500).send('Server Error.')
+//       }
+//       // console.log(JSON.parse(data));
+//       res.render('index.html', {
+//         colors: [
+//           'red',
+//           'yellow',
+//           'blue',
+//           'green'
+//         ],
+//         students: JSON.parse(data).students
+//       })
+//     })
+//   })
+//   app.get('/students/new', (req, res) => {
+
+//   })
+// }
 
